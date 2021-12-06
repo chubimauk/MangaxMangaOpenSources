bf387b4522e712fa0b4a0afe202b5a483f66500c
xOnlyFadi <63213308+xOnlyFadi@users.noreply.github.com>
2021-12-06 13:01:56 -0700

    Update toonilycom.js
    removed a spaces on artist

--- sources for users/sources-dev/toonilycom/toonilycom.js
+++ sources for users/sources-dev/toonilycom/toonilycom.js
@@ -238,7 +238,7 @@ module.exports = class ToonilyCom extends Source  {
         let thumbnai = $('div.summary_image img').attr('data-src');
         let table = $('div.summary_content');
         let author = $('div.author-content > a').text();
-        let artist = $('div.artist-content > a').text();  
+        let artist = $('div.artist-content > a').text();
         let status = $('div.post-status div.summary-content').text().toUpperCase().trim();
         if(status.includes("\nONGOING") || status.includes("\nCOMPLETED") || status.includes("\nCANCELED") || status.includes("\nOn Hiatus")){
             status = this.substringAfterLast("\n",status)

97e0bb50cfdc2917b5e6adea7a1099be9562dd96
xOnlyFadi <63213308+xOnlyFadi@users.noreply.github.com>
2021-12-06 17:41:09 -0700

    Removing the notes

--- sources for users/sources-dev/toonilycom/toonilycom.js
+++ sources for users/sources-dev/toonilycom/toonilycom.js
@@ -133,13 +133,11 @@ module.exports = class ToonilyCom extends Source  {
         var name = chapterAElement.text().trim();
         var scanlator = "";
         var date_upload = $('span').last().text().trim();
-
         var volumeNumber = '';
         var chapterNumber = '';
         const regex = RegExp(/\b\d+\.?\d?\b/g);
         if (name != null){
             var numbers = name.match(regex);
-            //console.log(`title numbers -- , ${numbers}, name -- , ${name}`);
             if (numbers != null){
                 if(numbers.length > 0){
                     var indexOfFirstNumber = name.indexOf(numbers[0]);
@@ -157,43 +155,34 @@ module.exports = class ToonilyCom extends Source  {
                         chapterNumber = numbers[0];
                     }
                     else {
-                        //have not set the chapter number yet from the first number
                         if(numbers.length > 1){
                             if((name.startsWith('v') || name.startsWith('V'))){
-                                //first number is volume
                                 volumeNumber = numbers[0];
                                 chapterNumber = numbers[1];
                             }
                             else {
-                                //probably never happen but, in the case where first number is not after chapter and we don't start with v, just use the first number as chapter
                                 chapterNumber = numbers[0];
-                                //second number is useless
                             }
                         }
                         else {
-                            //if there's only one number, just assume it is the chapter
                             chapterNumber = numbers[0];
                         }
                     }
-                    
-                    //check if possibly Volume is listed after chapter (very rare e.g. Chapter 108.5: Volume 12 Omake -- Boku No Hero Academia)
                     if(chapterNumber != '' && numbers.length > 1 && volumeNumber == ''){
-                        //chapter set, volume not set and we have one more number
                         if (name.includes('volume') || name.includes('Volume')){
-                            //fuck it just use the second number as the volume then
                             volumeNumber = numbers[1];
                         }
                     }
                 }
                 else {
-                    chapterNumber = "?"; //no numbers at all
+                    chapterNumber = "?";
                 }
             }
             else {
                 chapterNumber = "?";
             }
         } else {
-            chapterNumber = "?"; //no name, no chapter
+            chapterNumber = "?";
         }
         return super.chapter(url, "English", volumeNumber, chapterNumber, name, date_upload, scanlator);
     }
